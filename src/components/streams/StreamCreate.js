590cc3dcf23c9842e2b1d637b58561bc9136cb2d
Matthew Francis <mattfrancis888@gmail.com>
2020-04-30 23:54:44 -0400

    Designed Create Stream form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,57 +2,58 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 //reduxForm acts like connect()
 
-const StreamCreate = (props) => {
-    const renderError = ({ error, touched }) => {
+class StreamCreate extends React.Component {
+    renderError = ({ error, touched }) => {
         if (touched && error) {
             //Touched (for input) will be false at first
             //When clicked and then clicked otuside of the input, it will be true
-            return <div>{error}</div>;
+            return <div className="errorText">{error}</div>;
         }
     };
 
-    const renderInput = ({ input, label, meta }) => {
-        console.log(meta);
+    renderInput = ({ input, label, meta }) => {
         //"component" automatically passes props to argument, it has {input properties and meta properties}
         //"label" automatically passes props to arguments
         return (
             <div>
                 <label>{label}</label>
-                <input {...input} autoComplete="off" />
-                {renderError(meta)}
+                <input className="createInputs" {...input} autoComplete="off" />
+                {this.renderError(meta)}
             </div>
         );
         //{..input} is shortcut for redux-form; where you take all the input from "component's" props and pass it as
         //props to <input>
     };
 
-    const onSubmit = (formValues) => {
+    onSubmit = (formValues) => {
         //event.preventDefault()
         //Redux automaticlaly calls it with handleSubmit
         //form values are the values from the fields that redux-form automatiacally passes
         //after clicking the submit button
         //console.log(formValues);
     };
-
-    return (
-        <React.Fragment>
-            <div>StreamCrate</div>
-            <form onSubmit={props.handleSubmit(onSubmit)}>
-                <Field
-                    name="title"
-                    component={renderInput}
-                    label="Enter Title"
-                />
-                <Field
-                    name="description"
-                    component={renderInput}
-                    label="Enter Description"
-                />
-                <button>Submit</button>
-            </form>
-        </React.Fragment>
-    );
-};
+    render() {
+        return (
+            <React.Fragment>
+                <div className="createTitle">Create Stream</div>
+                <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                    <div className="createFormSection">
+                        <h1> Enter Title </h1>
+                        <Field name="title" component={this.renderInput} />
+                    </div>
+                    <div className="createFormSection">
+                        <h1> Enter Description</h1>
+                        <Field
+                            name="description"
+                            component={this.renderInput}
+                        />
+                    </div>
+                    <button className="blueBottom">Submit</button>
+                </form>
+            </React.Fragment>
+        );
+    }
+}
 
 const validate = (formValues) => {
     //MUST BE NAMED VALIDATE! Other names would be ignored by reduxForm(..)
@@ -70,7 +71,6 @@ const validate = (formValues) => {
     return errors;
     //Erors is going to be passed to renderInput's meta
 };
-
 export default reduxForm({
     form: "streamCreate",
     validate,

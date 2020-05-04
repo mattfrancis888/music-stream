30271c50cbf4ad29558a7ed168d3c024eed75df8
Matthew Francis <mattfrancis888@gmail.com>
2020-05-03 22:56:01 -0400

    When the edit button is clicked in StreamList. It will bring user to the edit page. It will show the title. Also able to go to the edit url right away without clicking the edit button.

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
+const StreamEdit = (props) => {
+    useEffect(() => {
+        props.fetchStream(props.match.params.id);
+        //Fetch the current stream first.
+        //If we dont and user enters /streams/edit, the state would be empty!
+    }, []);
+    console.log(props);
+    //Because we use <route> there are several props that are automatically passed into the componenet
+    if (!props.stream) {
+        return <div>Loading... </div>;
+        //When we first load the component state would be undefined, but then componenet will re-render again after the data for state is retrieved
+    }
+    return <div>{props.stream.title}</div>;
+};
 
-const StreamEdit = () =>{
-    return <div>StreamCrate</div>
-}
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    //mapstateToProps aslso has a default param, ownProps
+    //gets the props of the component
+    return { stream: state.streams[ownProps.match.params.id] };
+    //params represent the params of the URL
+    //gets the stream that has matching id, this can be done due to how we structured the state with lodash's mapKeys; eg; x:{}'!
+};
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -17,9 +17,9 @@ export default (state = {}, action) => {
         case CREATE_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case FETCH_STREAM:
-            return { ...state, [action.payload.id]: action.payload.id };
+            return { ...state, [action.payload.id]: action.payload };
         case EDIT_STREAM:
-            return { ...state, [action.payload.id]: action.payload.id };
+            return { ...state, [action.payload.id]: action.payload };
         case DELETE_STREAM:
             return _.omit(state, action.payload);
         //payload is the ID itself
